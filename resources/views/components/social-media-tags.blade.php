4118edd5018fe99bcafa345612145511268978aa
leicaflorian <florian.leica@gmail.com>
2022-06-03 01:06:06 +0200

    Fixes OG image

--- resources/views/components/social-media-tags.blade.php
+++ resources/views/components/social-media-tags.blade.php
@@ -2,11 +2,11 @@
       content="@yield("page-title") | Biologica Disinfestazioni - Professionisti nella provincia di Vicenza, Treviso e Padova">
 <meta property="og:site_name"
       content="Biologica Disinfestazioni - Professionisti nella provincia di Vicenza, Treviso e Padova">
-<meta property="og:url" content="https://www.biologicadisinfestazioni.it/">
+<meta property="og:url" content="https://www.biologicadisinfestazioni.it">
 <meta property="og:description" content="@yield("page-description")">
 <meta property="og:type" content="@yield("og_type", 'website')">
 <meta property="og:image"
-      content='url("{{asset("images/logo_wide.jpg")}}")'>
+      content='{{asset("images/logo_wide.jpg")}}'>
 
 <meta name="twitter:title"
       content="@yield("page-title") | Biologica Disinfestazioni - Professionisti nella provincia di Vicenza, Treviso e Padova">
